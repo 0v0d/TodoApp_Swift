4e628371645f835afd1b277e82c48678f616a0b2
0v0d <legolesia@gmail.com>
2024-12-17 18:56:47 +0900

    UIの調整

--- TodoApp/View/TaskList/Component/TaskRow.swift
+++ TodoApp/View/TaskList/Component/TaskRow.swift
@@ -13,14 +13,18 @@ struct TaskRow: View {
         HStack {
             Image(systemName: "document.fill")
                 .fontWeight(.bold)
+                .foregroundColor(.secondary)
 
             Text(task.title)
                 .font(.callout)
                 .lineLimit(1)
+                .truncationMode(.tail)
 
             Spacer()
 
             TaskStatusText(status: task.status, fontSize: .body)
+                .layoutPriority(1)
         }
+        .padding(.vertical, 1)
     }
 }
